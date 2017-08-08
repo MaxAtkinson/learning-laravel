e4ac8df2c15fce4667bb9bc3ee732d984de29826
Max Atkinson <Max@Maxs-MacBook-Pro.local>
2017-08-08 20:36:49 +0100

    added some ORM stuff

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use \App\Card;
 
 class CardsController extends Controller
 {
     public function index() {
-        $cards = DB::table('cards')->get();
+        $cards = Card::all();
         return view('cards.index')->withCards($cards);
+    }
+
+    public function show(Card $card) {
+        return $card;
     }
 }
